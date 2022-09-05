04742d93549e7214c0c06c8f913d9275ff6f1df5
user <user@DESKTOP-2PGRMF4>
2022-09-05 13:32:05 +0900

    sdsds

--- JAVA0905/src/access_modifier/Person.java
+++ JAVA0905/src/access_modifier/Person.java
@@ -3,7 +3,7 @@ package access_modifier;
 public class Person {
 	//외부에서 멤버변수에 접근이 불가능하게함
 	private String name;
-	private int age = 20;
+	private int age;
 	
 	public String getName() {
 		return name;
